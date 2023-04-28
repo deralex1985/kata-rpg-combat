9cc1e222dd730e7b4657e13fd0fb0097543253fb
adrian <adrian.sommer@css.ch>
2023-04-28 10:00:28 +0200

    added first test

---
+++ src/main/java/CombatCharacter.java
@@ -0,0 +1,29 @@
+public class CombatCharacter {
+    private int health;
+    private int level;
+    private boolean alive;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
---
+++ src/main/java/CombatCharacterTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CombatCharacterTest {
+
+    @Test
+    void shouldReturnCorrectInitializedClass() {
+        CombatCharacter testee = new CombatCharacter();
+
+        assertEquals(1000, testee.getHealth());
+        assertEquals(1, testee.getLevel());
+        assertTrue(testee.isAlive());
+    }
+}
